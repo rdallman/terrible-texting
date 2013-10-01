ff565224df89e2f6e47ec15fa376e92ff4e9682b
root <root@bt.(none)>
2013-10-01 09:40:59 -0400

    it is done

--- hw2.c
+++ hw2.c
@@ -1,46 +1,102 @@
+/*
+ * Reed Allman
+ * reed@auburn.edu
+ * man and stackoverflow are my only friends :(
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
-char* readFile(char* n);
+char* readFile(char* name);
+char* decryptMsg(char* emsg);
 
 typedef struct Node {
   struct Node* next;
   char c;
 } Node;
 
-typedef struct List {
-  Node* head;
+//place the node at <pos> at head of list, then return it
+Node* popIt(Node** list, int pos) {
+  Node *curr, *prev;
+  prev = NULL;
 
-  Node* (*popAtAndPush) (struct List* l, int pos);
-  void (*insert) (struct List* l, char c);
-} List;
-
-Node* popAtAndPush(List* l, int pos) {
   int i = 0;
-  Node* pn = l->head;
-  Node* n = pn;
-  while (i++ < pos) {
-    /*printf("%c", n->c);*/
-    pn = n;
-    n = n->next;
+  for ( curr = *list;
+        curr != NULL;
+        i++, prev = curr, curr = curr->next) {
+    //this is the node we are looking for
+    if (i == pos) {
+      if (prev == NULL) {
+      } else {
+        prev->next = curr->next;
+        curr->next = *list;
+        *list = curr;
+      }
+      return *list;
+    }
   }
-  /*printf("%c", n->c);*/
-  pn->next = n->next;
-  n->next = l->head;
-  l->head = n;
-  return n;
 }
 
-void insert(List* l, char c) {
-  Node* n = malloc(sizeof(Node));
+void insert(Node** list, char c) {
+  Node* n = (Node*)malloc(sizeof(Node));
   n->c = c;
-  n->next = l->head;
-  l->head = n;
-  /*printf("\nhead:%c", l->head->c);*/
+  n->next = *list;
+  *list = n;
 }
 
+char* decryptMsg(char *emsg) {
+  char *dmsg = malloc(sizeof(emsg));
+  Node* list = malloc(sizeof(Node));
+
+  size_t n = 0;
+  size_t m = 0;
+  size_t l = 0;
+
+  while (emsg[n]) {
+    if (isdigit(emsg[n])) {
+      //just getting an int from a char[], nothing to see here
+      char* num;
+      num = malloc(1024);
+      size_t z = 0;
+      while(isdigit(emsg[n])) {
+        num[z++] = emsg[n++];
+      }
+      dmsg[m++] = popIt(&list, atoi(num))->c;
+    } else {
+      //just put the letter
+      dmsg[m++] = emsg[n];
+      insert(&list, emsg[n++]);
+    }
+    n++;
+  }
+  dmsg[m] = '\0';
+  return dmsg;
+}
+
+//reads file <name> char by char
+char* readFile(char* name) {
+  FILE *file = fopen(name, "rt");
+  char *msg;
+  msg = malloc(1000 * sizeof(char));
+  size_t n = 0;
+  int c;
+
+  if (file == NULL) {
+    fprintf(stderr, "bad file: %s", name);
+    exit(1);
+  }
+
+  while ((c = fgetc(file)) != EOF) {
+    msg[n++] = (char) c;
+  }
+  msg[n] = '\0';
+
+  return msg;
+}
+
+//let the magic happen
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     fprintf(stderr, "Usage: %s file.txt", argv[0]);
@@ -50,57 +106,8 @@ int main(int argc, char *argv[]) {
   char* emsg = readFile(argv[1]);
   printf("Encrypted Message:\n%s\n", emsg);
 
-  char* dmsg;
-  dmsg = malloc(sizeof(emsg));
+  char* dmsg = decryptMsg(emsg);
+  printf("Decrypted Message:\n%s\n", dmsg);
 
-  List list;
-  list.head = NULL;
-  list.popAtAndPush = &popAtAndPush;
-  list.insert = &insert;
-
-  size_t n = 0;
-  size_t m = 0;
-  size_t l = 0;
-
-  while (emsg[n]) {
-    if (isdigit(emsg[n])) {
-      char* num;
-      num = malloc(1024);
-      size_t z = 0;
-      while(isdigit(emsg[n])) {
-        num[z++] = emsg[n++];
-      }
-      dmsg[m++] = list.popAtAndPush(&list, atoi(num))->c;
-      printf("%c", dmsg[m-1]);
-    } else {
-      /*printf("%c", emsg[n]);*/
-      dmsg[m++] = emsg[n];
-      list.insert(&list, emsg[n++]);
-      /*printf("%c", emsg[n]);*/
-    }
-    n++;
-  }
-  dmsg[m] = '\0';
-  /*printf("%s", dmsg);*/
-}
-
-char* readFile(char* name) {
-  FILE *file = fopen(name, "rt");
-  char *msg;
-  size_t n = 0;
-  int c;
-
-  if (file == NULL) {
-    fprintf(stderr, "bad file: %s", name);
-    exit(1);
-  }
-
-  msg = malloc(1000 * sizeof(char));
-
-  while ((c = fgetc(file)) != EOF) {
-    msg[n++] = (char) c;
-  }
-  msg[n] = '\0';
-
-  return msg;
+  return 1;
 }
